a9324f9fa7cfd1aebdb6fe8cefa14111b5b5739d
Dwain06 <dwain.mobile@gmail.com>
2022-05-02 15:22:28 +0200

    fix (modal): add error messages and labels

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,11 +17,23 @@ const modalClose = document.querySelector(".close");
 const prenom = document.getElementById("first");
 const nom = document.getElementById("last");
 const mail = document.getElementById("email");
+const naissance = document.getElementById("birthdate");
+const quantite = document.getElementById("quantity");
+// const tournois = document.querySelectorAll(".checkbox-input[type=radio]");
+const cgu = document.getElementById("checkbox1");
 
-const checkbox1 = document.getElementById("checkbox1");
+// const checkbox1 = document.getElementById("checkbox1");
 
-// RegExp
-// let formatMail = new RegExp("^[a-zA-Z0-9_-.]+[@]{1}[a-zA-Z0-9_-.]+[.]{1}[a-zA-Z]{2,5}$", "g");
+// Messages d'erreur
+
+let error = {
+  prenom: 'Veuillez saisir votre prénom avec au moins 2 caractères',
+  nom: 'Veuillez saisir votre nom avec au moins 2 caractères',
+  mail: 'Veuillez saisir un email valide',
+  quantite: 'Veuillez indiquer un nombre',
+  tournois: 'Veuillez cocher une case',
+  cgu: 'Merci d\'accepter les conditions générales'
+}
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -39,59 +51,104 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-
+// Erreurs sur éléments parents
 function unvalidInput(element, message) {
 	element.parentNode.setAttribute("data-error-visible", true);
 	element.parentNode.setAttribute("data-error", message);
 }
+
+// Suppression erreurs
 function validInput(element) {
 	element.parentNode.removeAttribute("data-error-visible");
 	element.parentNode.removeAttribute("data-error");
 }
 
+// Conditions données valides
 function isPrenomValid() {
-  return prenom.value !== null && prenom.value.length >= 2;
-}
+    return prenom.value !== null && prenom.value.length >= 2;
+  }
 
+function isNomValid() {
+    return nom.value !== null && nom.value.length >= 2;
+  }
+
+function isMailValid() {
+  let regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+	return regex.test(mail.value);
+  }
+
+function isQuantiteValid() {
+  let regex = /^[0-9]+$/;
+	return regex.test(quantite.value);
+  }
+
+// function isTournoisValid() {
+//   for (let eltTournois of tournois) {
+//     if (eltTournois.checked) {
+//       return true;
+//     }
+// }
+
+// function isTournoisValid() {
+// 	for (let radio of tournois) {
+// 		if (radio.checked) 
+//   }
+//   return false;
+// }
+
+
+function isCguValid() {
+	return cgu.checked;
+  }
+
+
+// Validation du formulaire
 function validateForm() {
   console.warn("validate")
   let isValid = true;
+
 	if (!isPrenomValid()) {
     isValid = false;
-		unvalidInput(prenom, "Veuillez entrer un prénom");
+		unvalidInput(prenom, error.prenom);
 	} else {
     validInput(prenom);
   }
 
+  if (!isPrenomValid()) {
+    isValid = false;
+		unvalidInput(nom, error.nom);
+	} else {
+    validInput(nom);
+  }
+
+  if (!isMailValid()) {
+    isValid = false;
+		unvalidInput(mail, error.mail);
+	} else {
+    validInput(mail);
+  }
+
+  if (!isQuantiteValid()) {
+    isValid = false;
+		unvalidInput(quantite, error.quantite);
+	} else {
+    validInput(quantite);
+  }
+
+  // if (!isTournoisValid()) {
+  //   isValid = false;
+	// 	unvalidInput(tournois, error.tournois);
+  //   console.warn("etape1")
+	// } else {
+  //   validInput(tournois);
+  // }
+
+  if (!isCguValid()) {
+    isValid = false;
+		unvalidInput(cgu, error.cgu);
+	} else {
+    validInput(cgu);
+  }
+
   return isValid;
 }
-
-
-// function validate() {
-//   if (prenom.value == "") {
-//     alert("Veuillez remplir votre prénom");
-//     prenom.focus();
-//     return false;
-//   } if (prenom.value.length < 2) {
-//     alert("Le prénom doit contenir au moins 2 caractères");
-//     prenom.focus();
-//     return false;
-//   } if (nom.value == "") {
-//     alert("Veuillez remplir votre nom");
-//     prenom.focus();
-//     return false;
-//   } if (nom.value.length < 2) {
-//     alert("Le nom doit contenir au moins 2 caractères");
-//     prenom.focus();
-//     return false;
-//   } if (mail.value != "" && mail.match(formatMail)) {
-//     alert("Veuillez saisir une adresse mail valide");
-//     prenom.focus();
-//     return false;
-//   } if (!checkbox1.checked) {
-//     alert("Veuillez accepter les CGU");
-//     checkbox1.focus();
-//     return false;
-//   }
-//   return true;
-// }
